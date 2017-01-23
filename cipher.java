5dad4fb724642fba8351759e30852b2ed7614a61
RityZhang <xinyuanzhang@umail.ucsb.edu>
2017-01-23 12:46:28 -0800

    Create cipher.java

---
+++ cipher.java
@@ -0,0 +1,7 @@
+import java.io.*;
+public class cipher{
+	public static final String str = "abcdefghijklmnopqrstuvwxyz";
+  public static String decrypt(String dtext){
+  cipher = cipher.toLowerCase();
+  String plaint="";
+  for(int i=0;i<cipher
